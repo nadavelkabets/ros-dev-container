dccb528c18ba7d184e958e152961f1b55f18395a
nadav <elnadav12@gmail.com>
2024-11-07 20:00:15 +0200

    remove old code dir if running script again

--- scripts/setup_dev_env.sh
+++ scripts/setup_dev_env.sh
@@ -11,6 +11,9 @@ sudo apt install -y docker.io docker-compose
 sudo usermod -aG docker $USER
 wget https://update.code.visualstudio.com/commit:0ee08df0cf4527e40edc9aa28f4b5bd38bbff2b2/linux-x64/stable -O ~/Downloads/vscode.tar.gz
 mkdir -p ~/.code
+if [ -d "~/.code" ]; then
+  sudo rm -r ~/.code
+fi
 tar --strip 1 -zxvf ~/Downloads/vscode.tar.gz -C ~/.code
 sudo rm /usr/bin/code
 sudo ln -s ~/.code/bin/code /usr/bin/code
