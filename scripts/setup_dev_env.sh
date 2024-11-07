f23736369e1ffbfe4c23d569792c0d4319228440
nadav <elnadav12@gmail.com>
2024-11-07 17:15:45 +0200

    improve setup scripts

--- scripts/setup_dev_env.sh
+++ scripts/setup_dev_env.sh
@@ -6,12 +6,13 @@ if [ $EUID == 0]; then
   exit 1
 fi
 
-sudo apt install docker.io
+sudo apt install -y docker.io docker-compose
 # enable user to access docker without sudo
 sudo usermod -aG docker $USER
 wget https://update.code.visualstudio.com/commit:0ee08df0cf4527e40edc9aa28f4b5bd38bbff2b2/linux-x64/stable -O ~/Downloads/vscode.tar.gz
 mkdir -p ~/.code
 tar --strip 1 -zxvf ~/Downloads/vscode.tar.gz -C ~/.code
+sudo rm /usr/bin/code
 sudo ln -s ~/.code/bin/code /usr/bin/code
 rm ~/Downloads/vscode.tar.gz
 
