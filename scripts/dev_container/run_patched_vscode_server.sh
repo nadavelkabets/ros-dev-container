e5d5a4fad8b34574a291108889d27feb417585ea
nadav <elnadav12@gmail.com>
2024-11-11 08:56:38 +0200

    pushd and popd not found in docker

--- scripts/dev_container/run_patched_vscode_server.sh
+++ scripts/dev_container/run_patched_vscode_server.sh
@@ -5,10 +5,10 @@ workspace_dir="/workspace"
 workspace_setup_script="${workspace_dir}/install/setup.sh"
 
 if [ ! -f ${workspace_setup_script} ] && [ "$(find $workspace_dir) -name package.xml)" != "" ]; then
-  pushd $workspace_dir
+  cd $workspace_dir
   echo "Building with colcon"
   colcon build --symlink-install
-  popd
+  cd -
 fi
 
 if [ -f $workspace_setup_script ]; then
