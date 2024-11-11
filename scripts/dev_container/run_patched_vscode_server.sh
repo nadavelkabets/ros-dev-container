c3522c77411ebc2d9302fed5b4c74d4302f89eee
nadav <elnadav12@gmail.com>
2024-11-11 09:30:15 +0200

    bugfix in command

--- scripts/dev_container/run_patched_vscode_server.sh
+++ scripts/dev_container/run_patched_vscode_server.sh
@@ -4,7 +4,7 @@
 workspace_dir="/home/user/workspace"
 workspace_setup_script="${workspace_dir}/install/setup.sh"
 
-if [ ! -f ${workspace_setup_script} ] && [ "$(find $workspace_dir) -name package.xml)" != "" ]; then
+if [ ! -f ${workspace_setup_script} ] && [ "$(find $workspace_dir -name package.xml)" != "" ]; then
   cd $workspace_dir
   echo "Building with colcon"
   colcon build --symlink-install
